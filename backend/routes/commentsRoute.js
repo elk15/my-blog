1994e22a721c424ffed2157f7939c51bcc09f691
elina <papadelina145@gmail.com>
2023-10-01 17:49:11 +0300

    Create comments controller

--- backend/routes/commentsRoute.js
+++ backend/routes/commentsRoute.js
@@ -1,22 +1,30 @@
 const express = require('express');
+const {
+    getComments,
+    getPostComments,
+    getComment,
+    createComment,
+    deleteComment,
+    updateComment,
+} = require('../controllers/commentsController');
 const router = express.Router();
 
 // GET all comments
-router.get('/', (req, res) => res.json({msg:"All comments"}));
+router.get('/', getComments);
 
 //GET comments of a post
-router.get('/replyingTo/:id', (req, res) => res.json({msg:"All comments of a post"}));
+router.get('/replyingTo/:id', getPostComments);
 
 //GET one comment
-router.get('/:id', (req, res) => res.json({msg:"One comment"}));
+router.get('/:id', getComment);
 
 //POST a new comment
-router.post('/', (req, res) => res.json({msg:"Comment created"}));
+router.post('/', createComment);
 
 //DELETE a comment
-router.delete('/:id', (req, res) => res.json({msg:"Comment deleted"}));
+router.delete('/:id', deleteComment);
 
 //UPDATE a comment
-router.patch('/:id', (req, res) => res.json({msg:"Comment updated"}));
+router.patch('/:id', updateComment);
 
 module.exports = router;
