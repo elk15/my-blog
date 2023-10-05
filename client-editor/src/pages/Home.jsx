d35c1068d0b5bfce52c5b9a6105b574c61a0b278
elina <papadelina145@gmail.com>
2023-10-05 18:49:23 +0300

    Fetch posts from server

--- client-editor/src/pages/Home.jsx
+++ client-editor/src/pages/Home.jsx
@@ -1,4 +1,27 @@
+import { useEffect } from "react";
+import { useContext } from "react";
+import { PostsContext } from "../context/PostsContext";
+
 const Home = () => {
+    const {posts, dispatch} = useContext(PostsContext);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            fetch('http://localhost:3001/api/posts/')
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("server error");
+                }
+            })
+            .then((json) => dispatch({type: 'SET_POSTS', payload: json}))
+            .catch((err) => console.log(err));
+        }
+
+        fetchPosts();
+    }, [dispatch])
+
     return (
         <>
             <a href="" className="border border-neutral-300 p-2 rounded text-lg hover:bg-neutral-200">
