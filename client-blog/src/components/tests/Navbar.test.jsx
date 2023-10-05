7589db0483b6d4c5bb3b5ccd742140f7abc1c331
elina <papadelina145@gmail.com>
2023-10-05 11:42:20 +0300

    Remove test

--- client-blog/src/components/tests/Navbar.test.jsx
+++ client-blog/src/components/tests/Navbar.test.jsx
@@ -15,16 +15,6 @@ describe('Navigation Bar', () => {
         expect(title.textContent).toBe('Full Stack Tutorials');
     });
 
-    it('displays three nav links', () => {
-        render(<BrowserRouter>
-                  <NavBar />
-              </BrowserRouter>);
-  
-          const links = screen.getAllByRole('heading', {level: 2});
-  
-          expect(links.length).toBe(3);
-    });
-
     it('displays change theme button', () => {
         render(<BrowserRouter>
                   <NavBar />
