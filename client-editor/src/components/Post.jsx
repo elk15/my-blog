e2c9fb09b91447232f5c305807bc3f9cd38860a5
elina <papadelina145@gmail.com>
2023-10-05 19:13:06 +0300

    Style post component

--- client-editor/src/components/Post.jsx
+++ client-editor/src/components/Post.jsx
@@ -1,16 +1,23 @@
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
 
 const Post = ({post}) => {
     return (
-    <section>
-        <div>
-            <p>Created at: {post.createdAt}</p>
-            <p>Last updated at: {post.updatedAt}</p>
+    <section className='border border-neutral-300 p-3 rounded max-w-[500px] w-full'>
+        <div className='flex flex-col md:flex-row justify-between text-neutral-600'>
+            <p> 
+                <span className="font-semibold">Created at: </span> 
+                {format(new Date(post.createdAt), 'dd/MM/yy hh:ss')}
+            </p>
+            <p>
+                <span className="font-semibold">Last updated at: </span>
+                {format(new Date(post.updatedAt), 'dd/MM/yy hh:ss')}
+            </p>
         </div>
-        <h2>{post.title}</h2>
-        <p>{post.snippet}</p>
-        <span>{post.tags.map(tag => tag.toUpperCase() + " ")}</span>
-        <div>
+        <h2 className='text-xl font-semibold'>{post.title}</h2>
+        <p className='text-neutral-500'>{post.snippet}</p>
+        <span className='text-teal-500 font-semibold'>{post.tags.map(tag => tag.toUpperCase() + " ")}</span>
+        <div className='flex gap-3 underline text-neutral-600'>
             <a href="">Moderate Comments</a>
             <a href="">Update</a>
             <a href="">Delete</a>
