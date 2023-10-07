06bf575ace3de311e0aa6effc3a6ed1479ee6ba6
elina <papadelina145@gmail.com>
2023-10-07 20:52:20 +0300

    Style comment component

--- client-editor/src/components/Comment.jsx
+++ client-editor/src/components/Comment.jsx
@@ -4,14 +4,20 @@ import { format } from 'date-fns';
 const Comment = ({comment}) => {
 
     return (
-        <section>
-            <p>{comment.name}</p>
+        <section className='flex flex-col gap-3 border border-neutral-300 p-2 
+        rounded max-w-[400px] w-full text-neutral-600'>
+            <p className='text-lg font-semibold'>{comment.name}</p>
             <p>{comment.body}</p>
-            <div>
-                <button>Update</button>
-                <button>Delete</button>
+            <div className='flex flex-wrap gap-2'>
+                <button className='underline hover:bg-neutral-900'>
+                    Update
+                </button>
+                <button className='underline hover:bg-neutral-900'>
+                    Delete
+                </button>
                 <span>
-                    Created at: {format(new Date(comment.createdAt), 'dd/MM/yy')}
+                    <strong>Created at: </strong>
+                    {format(new Date(comment.createdAt), 'dd/MM/yy')}
                 </span>
             </div>
         </section>
