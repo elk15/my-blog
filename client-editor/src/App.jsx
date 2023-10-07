03f847f95cfec2de8e30bd967c3eefdeec63a21c
elina <papadelina145@gmail.com>
2023-10-07 10:13:34 +0300

    Make a create a post page

--- client-editor/src/App.jsx
+++ client-editor/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
+import CreatePost from './pages/CreatePost';
 import Home from './pages/Home';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <main className='flex-1 flex flex-col items-center gap-4 max-w-[800px] w-full'>
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/create' element={<CreatePost />}/>
         </Routes>
       </main>
     </BrowserRouter>
