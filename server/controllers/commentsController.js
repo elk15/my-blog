11f1a26dba69c32e50d4255fa37b80538a89dda5
elina <papadelina145@gmail.com>
2023-10-07 20:05:35 +0300

    Add validation for comments

--- server/controllers/commentsController.js
+++ server/controllers/commentsController.js
@@ -1,5 +1,6 @@
 const Comment = require('../models/commentsModel');
 const mongoose = require('mongoose');
+const { body, validationResult } = require("express-validator");
 
 const getComments = async (req, res) => {
     const comments = await Comment.find({}).sort({createdAt: -1});
@@ -23,31 +24,39 @@ const getComment = async (req, res) => {
     res.status(200).json(comment);
 }
 
-const createComment = async (req, res) => {
-    const {name, body, replyingTo} = req.body;
+const createComment = [
+    body('name')
+        .trim()
+        .notEmpty()
+        .withMessage('Name is required')
+        .isLength({max: 50})
+        .withMessage('Name cannot exceed 50 characters')
+        .escape(),
+    body('body', 'Comment is required')
+        .trim()
+        .notEmpty()
+        .escape(),
+    async (req, res) => {
+        const errors = validationResult(req);
 
-    let emptyFields = []
+        if (!errors.isEmpty()) {
+            let emptyFields = [];
+            errors.array().forEach(err => {
+                emptyFields.push(err.path);
+            })
+            res.status(400).json({errors: errors.array(), emptyFields});
+            return;
+        }
 
-    if (!name) {
-        emptyFields.push('name');
-    }
-
-    if (!body) {
-        emptyFields.push('body');
-    }
-
-    if(emptyFields.length > 0) {
-        return res.status(400).json({ error: 'Please fill in all the fields', emptyFields})
-    }
-
-    try {
-        const comment = await Comment.create({name, body, replyingTo});
+        try {
+            const {name, body, replyingTo} = req.body;
+            const comment = await Comment.create({name, body, replyingTo});
         res.status(200).json(comment);
-    } catch(err) {
-        res.status(400).json({error: err.message});
+        } catch(err) {
+            res.status(400).json({errors: [{msg: err.message}]});
+        }
     }
-    
-}
+]
 
 const deleteComment = async (req, res) => {
     const {id} = req.params;
@@ -65,23 +74,37 @@ const deleteComment = async (req, res) => {
     res.status(200).json(comment);
 }
 
-const updateComment = async (req, res) => {
-    const {id} = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({error: 'No such comment'});
+const updateComment = [
+    body('name')
+        .trim()
+        .notEmpty()
+        .withMessage('Name is required')
+        .isLength({max: 50})
+        .withMessage('Name cannot exceed 50 characters')
+        .escape(),
+    body('body', 'Comment is required')
+        .trim()
+        .notEmpty()
+        .escape(),
+    async (req, res) => {
+        const errors = validationResult(req);
+    
+        if (!errors.isEmpty()) {
+            res.status(400).json({errors: errors.array()});
+            return;
+        }
+    
+        try {
+            const {id} = req.params;
+            const comment = await Comment.findOneAndUpdate({_id: id}, {
+                ...req.body
+            })
+            res.status(200).json(comment);
+        } catch(err) {
+            res.status(400).json({errors: [{msg: err.message}]});
+        }
     }
-
-    const comment = await Comment.findOneAndUpdate({_id: id}, {
-        ...req.body
-    });
-
-    if (!comment) {
-        return res.status(404).json({error: 'No such comment'});
-    }
-
-    res.status(200).json(comment);
-}
+]
 
 module.exports = {
     getComments,
