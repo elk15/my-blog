ea92adac94a2e7c808317c3a017d58e110ea9f3d
elina <papadelina145@gmail.com>
2023-10-07 10:06:59 +0300

    Make title clickable

--- client-editor/src/components/NavBar.jsx
+++ client-editor/src/components/NavBar.jsx
@@ -1,8 +1,10 @@
+import {Link} from 'react-router-dom';
+
 const NavBar = () => {
     return (
         <header className="w-full flex justify-center p-3 border mb-2">
             <div className="flex justify-between max-w-[800px] w-full items-center">
-                <h1 className="font-semibold text-xl">Blog Editor</h1>
+                <h1 className="font-semibold text-xl"><Link to="/">Blog Editor</Link></h1>
                 <button className="bg-stone-900 text-white py-1 px-2 rounded text-lg">
                     Log In
                 </button>
