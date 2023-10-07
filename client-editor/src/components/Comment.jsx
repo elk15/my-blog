755707bb5473d230a51502f58bc1d1aae01d5691
elina <papadelina145@gmail.com>
2023-10-07 22:16:55 +0300

    Add option to delete comments

--- client-editor/src/components/Comment.jsx
+++ client-editor/src/components/Comment.jsx
@@ -1,7 +1,27 @@
 import PropTypes from 'prop-types';
 import { format } from 'date-fns';
+import { useState } from 'react';
+import { useContext } from 'react';
+import { CommentsContext } from '../context/CommentsContext';
 
 const Comment = ({comment}) => {
+    const {dispatch} = useContext(CommentsContext);
+    const [confirmDelete, setConfirmDelete] = useState(false);
+
+    const handleDelete = async () => {
+        try {
+            const response = await fetch(`http://localhost:3001/api/comments/${comment._id}`, {
+            method: 'DELETE',
+            })
+            const json = await response.json();
+
+            if (response.ok) {
+                dispatch({type: 'DELETE_COMMENT', payload: json});
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     return (
         <section className='flex flex-col gap-3 border border-neutral-300 p-2 
@@ -9,16 +29,36 @@ const Comment = ({comment}) => {
             <p className='text-lg font-semibold'>{comment.name}</p>
             <p>{comment.body}</p>
             <div className='flex flex-wrap gap-2'>
-                <button className='underline hover:text-neutral-900'>
-                    Update
-                </button>
-                <button className='underline hover:text-neutral-900'>
-                    Delete
-                </button>
-                <span>
-                    <strong>Created at: </strong>
-                    {format(new Date(comment.createdAt), 'dd/MM/yy')}
-                </span>
+                {confirmDelete ?
+                <>
+                    <p className='font-semibold'>
+                        Are you sure you want to delete this comment?
+                    </p>
+                    <button className='underline text-red-500'
+                    onClick={handleDelete}>
+                        Yes
+                    </button>
+                    <button className='underline'
+                    onClick={() => setConfirmDelete(false)}>
+                        No
+                    </button>
+                </>
+                :
+                <>
+                    <button className='underline hover:text-neutral-900'>
+                        Update
+                    </button>
+                    <button className='underline hover:text-neutral-900'
+                    onClick={() => setConfirmDelete(true)}>
+                        Delete
+                    </button>
+                    <span>
+                        <strong>Created at: </strong>
+                        {format(new Date(comment.createdAt), 'dd/MM/yy')}
+                    </span>
+                </>
+                }
+                
             </div>
         </section>
     )
