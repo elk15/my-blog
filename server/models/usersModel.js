00f0f26ef7def03164f3a239b103ab7024153e16
elina <papadelina145@gmail.com>
2023-10-09 19:04:43 +0300

    Create user model

---
+++ server/models/usersModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+userSchema.statics.signup = async function (email, password) {
+    const exists = await this.findOne({ email });
+
+    if (exists) {
+        throw Error('Email already in use');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({email, password: hash});
+
+    return user;
+}
+
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+
+    if (!user) {
+        throw Error('Incorrect email');
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+        throw Error('Incorrect password');
+    }
+
+    return user;
+}
+
+module.exports = mongoose.model('User', userSchema);
