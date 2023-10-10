23d75dba8524bb624f2d347ba01ae3205f8c232f
elina <papadelina145@gmail.com>
2023-10-10 10:48:27 +0300

    Create authorization context

--- client-editor/src/main.jsx
+++ client-editor/src/main.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 import { CommentsContextProvider } from './context/CommentsContext.jsx'
 import { PostsContextProvider } from './context/PostsContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthContextProvider>
     <PostsContextProvider>
     <CommentsContextProvider>
       <App />
     </CommentsContextProvider>
     </PostsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 )
