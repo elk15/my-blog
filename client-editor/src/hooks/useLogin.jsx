b484af7fbaa71cddce83031da9617adb00c7f5ce
elina <papadelina145@gmail.com>
2023-10-10 10:53:20 +0300

    Create login hook

---
+++ client-editor/src/hooks/useLogin.jsx
@@ -0,0 +1,39 @@
+import {  useState } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+export const useLogin = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const {dispatch} = useContext(AuthContext);
+
+    const login = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch('http://localhost:3001/api/users/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({email, password})
+        })
+
+        const json = await response.json();
+
+        if (!response.ok) {
+            setIsLoading(false);
+            setError(json.error);
+        }
+
+        if (response.ok) {
+            // save the user to local storage
+            localStorage.setItem('user', JSON.stringify(json));
+
+            // update auth context
+            dispatch({type: 'LOGIN', payload: json});
+
+            setIsLoading(false);
+        }
+    }
+
+    return { login, isLoading, error }
+}
