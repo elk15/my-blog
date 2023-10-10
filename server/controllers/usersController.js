8a2e6b7e5399b80c9baa5f21e247aecdebafbdb5
elina <papadelina145@gmail.com>
2023-10-10 09:16:16 +0300

    Create controller for users

---
+++ server/controllers/usersController.js
@@ -0,0 +1,85 @@
+const User = require('../models/usersModel');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require("express-validator");
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '1d'});
+}
+
+// login user
+const loginUser = [
+    body("email", "Email cannot be empty")
+        .trim()
+        .notEmpty()
+        .escape(),
+    body("password", "Password cannot be empty")
+        .trim()
+        .notEmpty()
+        .escape(),
+    async (req, res) => {
+        const {email, password} = req.body;
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.status(400).json({errors: errors.array()});
+            return;
+        } 
+        
+        try {
+            const user = await User.login(email, password);
+        
+            // create a token
+            const token = createToken(user._id);
+        
+            res.status(200).json({email, token});
+        } catch(err) {
+            res.status(400).json({errors: [{msg: err.message}]});
+        }
+    }
+]
+
+// signup user
+const signupUser = [
+    body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email cannot be empty")
+        .isEmail()
+        .withMessage("Email isn't valid")
+        .escape(),
+    body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("Password cannot be empty")
+        .isStrongPassword({
+            minLength: 8, 
+            minLowercase:1, 
+            minUppercase: 1, 
+            minNumbers:1,
+            minSymbols: 1,
+        })
+        .withMessage("Password not strong enough")
+        .escape(),
+    async (req, res) => {
+        const {email, password} = req.body;
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.status(400).json({errors: errors.array()});
+            return;
+        } 
+        
+        try {
+            const user = await User.signup(email, password);
+        
+            // create a token
+            const token = createToken(user._id);
+        
+            res.status(200).json({email, token});
+        } catch(err) {
+            res.status(400).json({errors: [{msg: err.message}]});
+        }
+    }
+]
+
+module.exports = { signupUser, loginUser };
